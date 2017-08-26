01fe714e5d65864247f48078ea4980cb5765f582
Petterson Alves <pettalves@gmail.com>
2017-08-26 16:27:03 -0300

    Add tale image

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Button from '../../components/Button'
 import Cloud from '../../components/Cloud'
 
+import tale1x from './tale@1x.png'
+import tale2x from './tale@2x.png'
+
 import './styles.css'
 
 class Home extends Component {
@@ -16,6 +19,13 @@ class Home extends Component {
             Quero me inscrever
           </Button>
         </main>
+        <img className="tale" 
+          srcSet={
+            `${tale1x} 1x,
+             ${tale2x} 2x`
+          }
+          src={tale1x}
+        />
         <Cloud />
       </section>
     )
