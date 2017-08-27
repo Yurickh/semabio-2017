644f5fbe3f7713d5d004f7532edd8fcbe15a3370
Petterson Alves <pettalves@gmail.com>
2017-08-27 15:04:05 -0300

    Create about page and create title component

---
+++ src/scenes/About/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Title from '../../components/Title'
+
+import './styles.css'
+
+class About extends Component {
+  render() {
+    return (
+      <section className="page-about">
+        <Title>
+          Sobre Nós
+        </Title>
+        <p>
+          Caros amigos, a execução dos pontos do programa agrega valor ao estabelecimento do investimento em
+           reciclagem técnica. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a
+           revolução dos costumes talvez venha a ressaltar a relatividade dos índices pretendidos. Assim
+           mesmo, o entendimento das metas propostas faz parte de um processo de gerenciamento do processo
+           de comunicação como um todo. Todavia, a estrutura atual da organização ainda não demonstrou
+           convincentemente que vai participar na mudança de todos os recursos funcionais envolvidos.
+        </p>
+      </section>
+    )
+  }
+}
+
+export default About
--- src/scenes/App/index.js
+++ src/scenes/App/index.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import Home from '../Home'
+import About from '../About'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
-        {/* <About />
-        <Team />
-        <Schedule />
-        <Features />
-        <Shop />
-        <Faq />
-        <PlacesToStay />
-        <Map />
-        <Contact />
-        <Partners /> */}
+        <About />
+        {// <Team />
+        // <Schedule />
+        // <Features />
+        // <Shop />
+        // <Faq />
+        // <PlacesToStay />
+        // <Map />
+        // <Contact />
+        // <Partners /> */
+      }
       </div>
     )
   }
