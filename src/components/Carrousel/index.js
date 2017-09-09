3e08124a6cc410557cfae3cf9397ba2213c60884
Yurick <yurick@flama.is>
2017-09-09 12:31:46 -0300

    Add basic functionality

--- src/components/Carrousel/index.js
+++ src/components/Carrousel/index.js
@@ -7,15 +7,56 @@ import './styles.css'
 const numOfPeople = 16
 
 class Carrousel extends Component {
-  createPeople() {
+  state = {
+    selected: 8
+  }
+
+  isSelected = item => item === this.state.selected
+
+  createPeople = () => {
     return (
       [...Array(numOfPeople)].map((value, i) => (
-        <div className="person" key={i}>
+        <div
+          style={this.styleFor(i)}
+          className={`person ${this.isSelected(i) ? '-centered' : ''}`}
+          key={i}
+        >
           {/* change key from i to value, eventually */}
-          <img alt="avatar" className="avatar" src={person} />
+          <img
+            alt="avatar"
+            src={person}
+            className='avatar'
+          />
         </div>
       ))
     )
+  }
+
+  styleFor = (index) => {
+    let offset = index - this.state.selected
+
+    if (offset < -numOfPeople/2) {
+      offset += numOfPeople
+    } else if (offset > numOfPeople/2) {
+      offset -= numOfPeople
+    }
+
+    return {
+      transform: `translateX(${offset * 240}px)`
+    }
+  }
+
+  moveToNext = () => {
+    const selected = (this.state.selected + 1) % numOfPeople
+    this.setState({ selected  })
+  }
+
+  moveToPrevious = () => {
+    const selected = (this.state.selected - 1) % numOfPeople
+
+    this.setState({
+      selected: selected < 0 ? numOfPeople : selected
+    })
   }
 
   render() {
@@ -27,6 +68,7 @@ class Carrousel extends Component {
         <div className="bar">
           <div className="middle-bar" />
         </div>
+        <button onClick={this.moveToNext}>CLICKME</button>
       </div>
     )
   }
